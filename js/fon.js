eccbd8515a90d5e313ad8335c08902101c3cdb25
Pavel <komdiv.java@gmail.com>
2024-09-22 00:20:40 +0300

    firstCommit

---
+++ js/fon.js
@@ -0,0 +1,7 @@
+//document.body.style.background = "yellow";
+//document.body.style.background = "green";
+document.body.style.background = 'url("img/wood.jpg")';
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundPosition = "inherit";
+document.body.style.backgroundSize = "cover";
+//document.body.style.backgroundSize = "100% 100%";
